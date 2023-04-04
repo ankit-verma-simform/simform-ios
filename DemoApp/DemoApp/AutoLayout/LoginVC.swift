a621da20226bbd8442b39d144b4eb9fb506ff93b
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-04-04 18:50:54 +0530

    added autolayout exercise files

---
+++ DemoApp/DemoApp/AutoLayout/LoginVC.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  DemoApp
+//
+//  Created by Ankit Verma on 11/04/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
