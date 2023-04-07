7c5b4c83e37331931c03592a61b245bb214f9c26
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-04-07 11:01:41 +0530

    completed sign in screens design given by mentor

---
+++ DemoApp/DemoApp/StartScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartScreenViewController.swift
+//  DemoApp
+//
+//  Created by Ankit Verma on 07/04/23.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
