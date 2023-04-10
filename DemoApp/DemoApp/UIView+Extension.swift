922cbe949c0827770479753f177a03670098ff88
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-04-10 18:32:29 +0530

    practiced adaptive layouts and sized classes, refactored constraints

--- DemoApp/DemoApp/UIView+Extension.swift
+++ DemoApp/DemoApp/UIView+Extension.swift
@@ -1,85 +1,85 @@
+////
+////  UIView+Extension.swift
+////  DemoApp
+////
+////  Created by Ankit Verma on 06/04/23.
+////
 //
-//  UIView+Extension.swift
-//  DemoApp
+//import UIKit
 //
-//  Created by Ankit Verma on 06/04/23.
+//extension UIView {
 //
-
-import UIKit
-
-extension UIView {
-
-    @IBInspectable
-    var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-        }
-    }
-
-    @IBInspectable
-    var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-
-    @IBInspectable
-    var borderColor: UIColor? {
-        get {
-            let color = UIColor.init(cgColor: layer.borderColor!)
-            return color
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
-        }
-    }
-
-    @IBInspectable
-    var shadowRadius: CGFloat {
-        get {
-            return layer.shadowRadius
-        }
-        set {
-
-            layer.shadowRadius = shadowRadius
-        }
-    }
-    @IBInspectable
-    var shadowOffset : CGSize{
-
-        get{
-            return layer.shadowOffset
-        }set{
-
-            layer.shadowOffset = newValue
-        }
-    }
-
-    @IBInspectable
-    var shadowColor : UIColor{
-        get{
-            return UIColor.init(cgColor: layer.shadowColor!)
-        }
-        set {
-            layer.shadowColor = newValue.cgColor
-        }
-    }
-    @IBInspectable
-    var shadowOpacity : Float {
-
-        get{
-            return layer.shadowOpacity
-        }
-        set {
-
-            layer.shadowOpacity = newValue
-
-        }
-    }
-}
+//    @IBInspectable
+//    var cornerRadius: CGFloat {
+//        get {
+//            return layer.cornerRadius
+//        }
+//        set {
+//            layer.cornerRadius = newValue
+//        }
+//    }
+//
+//    @IBInspectable
+//    var borderWidth: CGFloat {
+//        get {
+//            return layer.borderWidth
+//        }
+//        set {
+//            layer.borderWidth = newValue
+//        }
+//    }
+//
+//    @IBInspectable
+//    var borderColor: UIColor? {
+//        get {
+//            let color = UIColor.init(cgColor: layer.borderColor!)
+//            return color
+//        }
+//        set {
+//            layer.borderColor = newValue?.cgColor
+//        }
+//    }
+//
+//    @IBInspectable
+//    var shadowRadius: CGFloat {
+//        get {
+//            return layer.shadowRadius
+//        }
+//        set {
+//
+//            layer.shadowRadius = shadowRadius
+//        }
+//    }
+//    @IBInspectable
+//    var shadowOffset : CGSize{
+//
+//        get{
+//            return layer.shadowOffset
+//        }set{
+//
+//            layer.shadowOffset = newValue
+//        }
+//    }
+//
+//    @IBInspectable
+//    var shadowColor : UIColor{
+//        get{
+//            return UIColor.init(cgColor: layer.shadowColor!)
+//        }
+//        set {
+//            layer.shadowColor = newValue.cgColor
+//        }
+//    }
+//    @IBInspectable
+//    var shadowOpacity : Float {
+//
+//        get{
+//            return layer.shadowOpacity
+//        }
+//        set {
+//
+//            layer.shadowOpacity = newValue
+//
+//        }
+//    }
+//}
